becd97041dba7d65636340441156afc2ed2d5d50
Aarushi Agarwal <agarwalaarushi95@gmail.com>
2024-11-09 16:06:41 +0530

    first commit

---
+++ server/controllers/student-controller/student-courses-controller.js
@@ -0,0 +1,27 @@
+const StudentCourses = require('../../models/StudentCourses');
+
+
+const getCoursesByStudentId = async(req, res)=>{
+    try{
+        const {studentId} = req.params;
+        const studentBoughtCourses = await StudentCourses.findOne({
+            userId : studentId
+        })
+
+        res.status(200).json({
+            successs : true,
+            data : studentBoughtCourses.courses
+        })
+
+    }catch(error){
+        console.log(error);
+        
+        res.status(500).json({
+            success : false,
+            message : 'Some error occurred!'
+        })
+    }
+
+}
+
+module.exports = {getCoursesByStudentId}
